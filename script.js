8dead612ec65d0dd25a6cd4b9dafad2b57b52855
Ernesto Alonso <60942733+ernesto-code@users.noreply.github.com>
2023-03-03 16:56:19 -0500

    feat: layout updates and scripting building

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
+
+const headingBtn = document.getElementById('headings-btn')
+
+// Event listener adding
+headingBtn.addEventListener("click",(evt)=>{
+    console.log("showing headings")
+    activateStyle(evt.target)
+})
+
+//Event handlers
+
+
+// Functions
+const activateStyle = (elm) => {
+    elm.classList.add('toolkit-active-btn')
+}
+
+
+
+
+const codeToDOM = `<div class="layout-container">
+<div class="toolkit">
+    <button id = "headings-btn" class="centered btn-dark toolkit-btn" title="Show Headings"><!--i class="fa-light fa-heading"></i--> H</button>
+    <button id = "list-btn" class="centered btn-dark toolkit-btn" title="Show Lists"><!--i class="fa-light fa-heading"></i--> Ul</button>
+    <button class="centered btn-dark toolkit-btn" title="Currently Focused element while tabbing"><!--i class="fa-light fa-heading"></i--> F</button>
+</div>
+</div>`
+
+
+
+
 const bodyElement = document.querySelector('body')
 
 // Adding container for the identification divs
+
 const divContainer = document.createElement("div")
-divContainer.style.position = 'fixed'
+
+/*divContainer.style.position = 'fixed'
 divContainer.style.top = '0'
 divContainer.style.left = '0'
 divContainer.style.visibility = 'visible'
@@ -28,3 +61,5 @@ divContainer.appendChild(divIdentifier)
 // Insering container in DOM
 
 bodyElement.appendChild(divContainer)
+
+*/
